0224f95b7423c4bde0cbba303798a1a2b26de47a
wc <3413089620@qq.com>
2022-11-15 17:26:44 +0800

    add function of menu and main interface

---
+++ game_1/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from game_1.views import index, menu
+
+
+urlpatterns = [
+    path("", index, name = "index"),
+    path("menu/", menu, name = "menu")
+    ]
+
+
+
+
--- game_1/views.py
+++ game_1/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    line1 = '<h1 style="text-align:center;">Welcome to here!</h1>'
+    line2 = '<img src="https://img1.baidu.com/it/u=3424582540,3550213785&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=360"/>'
+    line4 = """
+    <div style="text-align:center; color:red;">
+    <a href="/menu/"> 菜单 </a>
+    </div>
+    """
+    
+    return HttpResponse(line1 + line4 + line2)
+
+def menu(request):
+
+    return HttpResponse('MENU')
+
